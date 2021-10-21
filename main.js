7d75009fdc4ccfc4807d89977bbbc394932a61f5
Mohammed-Zaky <learnmeprogramming@gmail.com>
2021-10-21 21:21:28 +0200

    Analog Clock

--- main.js
+++ main.js
@@ -6,6 +6,18 @@ function clock() {
   let seconds = date.getSeconds() / 60;
   let minutes = (seconds + date.getMinutes()) / 60;
   let hours = (minutes + date.getHours()) / 12;
+  // digital clock
+  let digitalHours = date.getHours() - 12;
+  let digitalMin = date.getMinutes();
+  
+  if (digitalHours < 10) {
+    digitalHours = "0" + digitalHours;
+  }
+  if (digitalMin < 10) {
+    digitalMin = "0" + digitalMin;
+  }
+  let digital = `${digitalHours}:${digitalMin}`;
+  time.innerHTML = digital;
   // play on elements
   play(hour, hours);
   play(min, minutes);
