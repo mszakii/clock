a4b3ccbad9b117a0a55ccbbf7e05158a46c913d5
Mohammed-Zaky <learnmeprogramming@gmail.com>
2021-10-22 10:22:03 +0200

    Make Transision And Make Js File More Readable

--- main.js
+++ main.js
@@ -6,27 +6,34 @@ function clock() {
   let seconds = date.getSeconds() / 60;
   let minutes = (seconds + date.getMinutes()) / 60;
   let hours = (minutes + date.getHours()) / 12;
+  
   // digital clock
   let digitalHours = date.getHours();
   if (digitalHours > 12) {
     digitalHours = digitalHours - 12;
   }
-  let digitalMin = date.getMinutes();
-  
   if (digitalHours < 10) {
     digitalHours = `0${digitalHours}`;
   }
+  
+  let digitalMin = date.getMinutes();
   if (digitalMin < 10) {
     digitalMin = `0${digitalMin}`;
   }
+  
   let digital = `${digitalHours}:${digitalMin}`;
-  time.innerHTML = digital
+  time.innerHTML = digital;
+  
   // play on elements
   play(hour, hours);
   play(min, minutes);
   play(sec, seconds);
+  
   // tick sound
   tick.play();
+  setTimeout(function() {
+    navigator.vibrate([15]);
+  }, 10);
 }
 
 function play(ele, rotate) {
