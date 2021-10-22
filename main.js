43f19f04d61862dadb7a976a9aa29c80dcaa697f
Mohammed-Zaky <learnmeprogramming@gmail.com>
2021-10-22 04:57:40 +0200

    add tick sound

--- main.js
+++ main.js
@@ -7,21 +7,26 @@ function clock() {
   let minutes = (seconds + date.getMinutes()) / 60;
   let hours = (minutes + date.getHours()) / 12;
   // digital clock
-  let digitalHours = date.getHours() - 12;
+  let digitalHours = date.getHours();
+  if (digitalHours > 12) {
+    digitalHours = digitalHours - 12;
+  }
   let digitalMin = date.getMinutes();
   
   if (digitalHours < 10) {
-    digitalHours = "0" + digitalHours;
+    digitalHours = `0${digitalHours}`;
   }
   if (digitalMin < 10) {
-    digitalMin = "0" + digitalMin;
+    digitalMin = `0${digitalMin}`;
   }
   let digital = `${digitalHours}:${digitalMin}`;
-  time.innerHTML = digital;
+  time.innerHTML = digital
   // play on elements
   play(hour, hours);
   play(min, minutes);
   play(sec, seconds);
+  // tick sound
+  tick.play();
 }
 
 function play(ele, rotate) {
